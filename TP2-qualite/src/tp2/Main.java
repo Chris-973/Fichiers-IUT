eda062f90007e5c536fff63dd1202d7890a56ca2
Christopher Marie-Angelique <christopher2211.angelique@gmail.com>
2022-09-15 21:47:12 +0200

    Ajout dossier

---
+++ TP2-qualite/src/tp2/Main.java
@@ -0,0 +1,16 @@
+package tp2;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+public static void main(String[] args) {
+	StrLen str;
+	str = new StrLen();
+	List<String> li = Arrays.asList("a", "bb", "ccc", "dddd", "eeeee");
+	StrLen.truncate(li, 3);
+	
+	
+}
+}
